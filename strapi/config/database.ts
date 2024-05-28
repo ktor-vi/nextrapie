845744a895518a56dd3a64039cab79dbc56416e5
ktorvi <kt@grauhaus.tech>
2024-05-28 04:06:55 +0000

    ci: droplet functioning

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -4,9 +4,9 @@ export default ({ env }) => ({
   connection: {
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapidb'),
-        user: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'nextrapie'),
+        user: env('DATABASE_USERNAME', 'nextrapie'),
+        password: env('DATABASE_PASSWORD', 'sisaulait'),
         ssl: env.bool("DATABASE_SSL", false) && {
           rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
